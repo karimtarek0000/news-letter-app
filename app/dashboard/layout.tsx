88ccd6e36c61791e64cf00979d0b2af8ae1b84a0
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-06-02 16:27:38 +0300

    Refactor

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,7 @@
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
 import type { ReactNode } from 'react'
 import DashboardHeader from '../_components/dashboard/DashboardHeader'
 
 async function Dashboardlayout({ children }: { children: ReactNode }) {
-  const { has } = await auth()
-  const hasPaidPlan = has({ plan: 'pro' }) || has({ plan: 'starter' })
-
-  // if (!hasPaidPlan) {
-  //   redirect('/#pricing')
-  // }
-
   return (
     <div className="min-h-screen flex flex-col">
       <DashboardHeader />
